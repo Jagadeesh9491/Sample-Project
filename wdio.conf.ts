8525898ed66b0f8ef4dc1c5da8e359402bc950d0
Jagadeesh9491 <48913917+Jagadeesh9491@users.noreply.github.com>
2023-08-02 20:01:34 +0530

    Update wdio.conf.ts

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -133,22 +133,22 @@ export const config: Options.Testrunner = {
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
     reporters: ['spec', 
-    // ['allure',
-    //     {
-    //         outputDir: 'allure-results',
-    //         disableWebdriverStepsReporting: true,
-    //         disableWebdriverScreenshotsReporting: false,
-    //     }
-    // ],
-    [
-      'junit',
-      {
-        outputDir: 'junit reports',
-        outputFileFormat: function (options) {
-          return `results-${options.cid}.xml`;
-        },
-      },
-    ],
+     ['allure',
+         {
+             outputDir: 'allure-results',
+             disableWebdriverStepsReporting: true,
+             disableWebdriverScreenshotsReporting: false,
+         }
+     ],
+    //[
+    //  'junit',
+    //  {
+    //    outputDir: 'junit reports',
+    //    outputFileFormat: function (options) {
+    //      return `results-${options.cid}.xml`;
+    //    },
+    //  },
+   // ],
  //  ],
 ],
 
